0eab173bcd6ebde888addc3aa39cbabaf897a53e
wamahiga <61947672+Wamahiga@users.noreply.github.com>
2022-03-22 12:48:58 +0300

    sparemart project

---
+++ aboutus.php
@@ -0,0 +1,55 @@
+<?php include('partials-front/menu.php'); ?>
+<div class = "about-section">
+<div class="text-center">
+  <h1>About Us</h1>
+</div>
+
+      <p>Sparemart is an autospares shop located in Nairobi along Kirinyaga Road.Our Main aim is to provide our esteemed customers with the best <br>
+          of the best quality spareparts with no hassle.We have areliable delivery service where you an order goods and pay via paybill once the <br>
+          items are delivered to your location of choice. <br> Incase of any  enquiries or comolaints you can reach us via the contacts and social media below:
+      </p>
+  <p>.</p>
+</div>
+
+
+<h2 style="text-align:center">Our contacts:</h2>
+<div class="tbl-full">
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: ;
+}
+</style>
+</head>
+<body>
+
+
+
+<table>
+  <tr>
+    <th>Phone Number</th>
+    <th>Email Adress</th>
+    <th>Socialmedia handles</th>
+  </tr>
+  <tr>
+    <td>0703360021</td>
+    <td>sparemartauto@gmail.com</td>
+    <td>Sparemart_auto</td>
+  </tr>
+
+</table>
+  </div>
+</div>
+
+<?php include('partials-front/footer.php'); ?>
---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2021 All rights reserved, Spare Mart. Developed By - <a href="#">Wamahiga Cynthia</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
